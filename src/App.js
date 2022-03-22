a481cbdcc2b0791c4c425fc2c93117121188f07d
shyaminid <shyamini0306@gmail.com>
2022-03-22 22:41:30 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { useState } from "react";
 
 function App() {
+  // for capturing input value from input box;
+  const [task, setTasks] = useState([]);
+
+  const onchangeInput = (event) => {
+    setTasks(event.target.value);
+  };
+  //for capturing task on Click of button;
+
+  const [taskAdding, setTaskAdding] = useState([]);
+  console.log("taskadding", taskAdding);
+
+  const onclickButton = () => {
+    setTaskAdding([...taskAdding, task]);
+  };
+  const buttonStyles = { width: "100px" };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="container">
+        <div className="inputrow">
+          <input
+            className="taskInput"
+            type="text"
+            onChange={onchangeInput}
+            placeholder="New Task..."
+          />
+
+          <button
+            variant="primary"
+            style={buttonStyles}
+            onClick={onclickButton}
+          >
+            Add Task
+          </button>
+        </div>
+        <div className="filter-contents">
+          <button>All</button>
+          <button>Active</button>
+          <button>Completed</button>
+        </div>
+        {taskAdding.map((tas) => (
+          <Taskadd t={tas} />
+        ))}
+
+        {/* {taskAdding.map((tas) => (
+          <Taskadd t={tas} />
+        ))} */}
+      </div>
+    </div>
+  );
+}
+
+function Taskadd({ t }) {
+  console.log("t", t);
+  const styles = {
+    backgroundColor: "pink",
+    width: "80%",
+    display: "flex",
+    flexDirection: "column",
+    margin: "8px",
+  };
+  const styles2 = {
+    display: "flex",
+    flexDirection: "row",
+  };
+  return (
+    <div style={styles}>
+      <div style={styles2}>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <input type="checkbox" value={t} />
+          {t}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
